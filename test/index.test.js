2bff6343dd1b887ac268c304aca6f665a516a549
kondareddy103 <kondareddy103@gmail.com>
2018-07-19 10:08:03 +0530

    Update index.test.js

--- test/index.test.js
+++ test/index.test.js
@@ -2,6 +2,7 @@ var request = require('supertest');
 var express = require('express');
  
 var app = require('../app');
+var app = require('../app');
  
 describe('Index Page', function() {
   it("renders successfully", function(done) {
